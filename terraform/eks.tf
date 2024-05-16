5bcbb583ba486f5c2afde9c3d8b855cec298140b
Gabriel Fontes <hi@m7.rs>
2024-05-15 22:32:28 -0300

    fix(k8s): move node group settings to default_node_group

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -11,15 +11,13 @@ module "eks" {
   subnet_ids               = module.vpc.private_subnets
   control_plane_subnet_ids = module.vpc.private_subnets
 
-  eks_managed_node_group_defaults = {
-    min_size     = 1
-    max_size     = 10
-    desired_size = 2
-    instance_types = ["t2.small"]
-  }
-
   eks_managed_node_groups = {
-    default_node_group = {}
+    default_node_group = {
+      min_size     = 1
+      max_size     = 10
+      desired_size = 2
+      instance_types = ["t2.small"]
+    }
   }
 
   enable_cluster_creator_admin_permissions = true
